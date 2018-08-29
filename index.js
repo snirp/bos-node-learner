393f306358113a6bb5ee11e77139c138f7f7c974
Roy Prins <prinsroy@gmail.com>
2018-08-29 18:51:37 +0200

    Lesson 03 initial

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+require('dotenv').config({path: 'variables.env'})
+ 
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Initial app listening on port 3000!'))
+app.listen(port, () => console.log(`App listening on port ${port}`))
