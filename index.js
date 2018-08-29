5de8556b4a6e64f0465ca29894f26b7824feace8
Roy Prins <prinsroy@gmail.com>
2018-08-29 23:11:32 +0200

    env variables and db connection

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+
+// Load .env variables into environment
 require('dotenv').config()
- 
+
+// Configure database 
+mongoose.connect(process.env.DATABASE)
+mongoose.Promise = global.Promise
+mongoose.connection.on('error', (err) => console.error(`DB connection error: ${err.message}`))
+
+const app = express()
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
